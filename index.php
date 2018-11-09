ad1605765abfb1f8a35b6578876f4e9d13b0a2ad
FrandeSUG <frandesugianto@gmail.com>
2018-11-09 15:41:14 +0900

    new

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Frande</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="main1.css">
+  <link rel="apple-touch-icon" sizes="57x57" href="item/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="item/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="item/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="item/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="item/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="item/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="item/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="item/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="item/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="item/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="item/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="item/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="item/favicon-16x16.png">
+  <link rel="manifest" href="item/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="item/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>  
+      <div class="ui menu large top fixed inverted">
+        <div class="ui container">
+        <div class="item">
+          <img src="item/logo.png">
+        </div>
+          <a class="item" href="#portfolio">Portfolio</a>
+          <a class="item" href="#resume">Resume</a>
+      </div>
+    </div>
+ <!-- Page Contents -->
+<div class="pusher">
+
+  <div class="ui inverted vertical masthead segment">
+    <div class="ui text container">
+      <h1 class="ui inverted header">
+        Introduction
+      </h1>
+      <h2>
+        WONG HOCK CHUAN SUGIANTO<br>専門学校東京クールジャパン<br>ゲーム総合学科ゲームプログラマー専攻
+      </h2>
+      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+    </div>
+  </div>
+
+
+  <a name="portfolio"></a> <!-- anchor -->
+  <div class="ui vertical stripe segment">
+      <div class="ui message container inverted">
+        
+        <h1 class="ui header inverted">Portfolio</h1>
+      </div>
+    <div class="ui stackable grid container">
+      <div class="row">
+        <div class="eight wide left floated column">
+          <img src="item/sample.png" class="ui large bordered rounded image">
+        </div>
+        <div class="eight wide column">
+          <h3 class="ui header">Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+          <div class="eight wide left floated column">
+          <img src="item/sample.png" class="ui large bordered rounded image">
+        </div>
+        <div class="eight wide column">
+          <h3 class="ui header">Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <a name="resume"></a> <!-- anchor -->
+  <div class="ui vertical stripe segment">
+      <div class="ui message container inverted">
+        
+        <h1 class="ui header inverted">Resume</h1>
+      </div>
+    <div class="ui stackable grid container">
+      <div class="row">
+        <div class="sixteen wide column">
+          <img src="item/resume.png" class="ui fluid bordered rounded image">
+        </div>
+      </div>
+      <div class="row">
+        <div class="center aligned column">
+          <a class="ui huge button" href="item/rirekisho.pdf">Download PDF</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+  <div class="ui inverted vertical footer segment">
+    <div class="ui container">
+      <div class="ui stackable inverted divided equal height stackable grid">
+        <div class="eight wide column">
+          <h2 class="ui inverted header">Links</h2>
+          <div class="ui inverted link list">
+            <a href="https://github.com/FrandeSUG" class="item">Github</a>
+            <a href="#" class="item">Twitter</a>
+          </div>
+        </div>
+          <div class="eight wide column">
+            <div class="ui inverted link list">
+              <a href="#top" class="item">BACK TO TOP</a>
+            <div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
+
+
+
+
